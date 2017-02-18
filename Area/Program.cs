924931a3097618e5106219049c45e4474325906a
Jim Baker <jim.k.baker@icloud.com>
2017-02-18 12:18:21 -0600

    additional solution to Studio Area

--- Area/Program.cs
+++ Area/Program.cs
@@ -14,12 +14,15 @@ namespace Area
 
             do
             {
-                Console.WriteLine("Please enter a positive radius: ");
+                Console.Write("Please enter a positive radius: ");
                 radius = Convert.ToDouble(Console.ReadLine());
+                // ...or...
+                // string input = Console.ReadLine();
+                // double radius = double.Parse(input);
             } while (radius < 0);
             
             area = 3.14 * radius * radius;
-            Console.WriteLine("The Area = " + area);
+            Console.WriteLine("The area of a cirle with a radius of " + radius + " is " + area);
 
             Console.ReadLine();
         }
