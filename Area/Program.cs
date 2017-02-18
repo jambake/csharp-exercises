7cf28f9040df48d7504b776d61cbbe680330b136
Jim Baker <jim.k.baker@icloud.com>
2017-02-18 10:47:55 -0600

    add helloworld and area programs

---
+++ Area/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Area = pi * r * r;
+            double area;
+            double radius;
+
+            do
+            {
+                Console.WriteLine("Please enter a positive radius: ");
+                radius = Convert.ToDouble(Console.ReadLine());
+            } while (radius < 0);
+            
+            area = 3.14 * radius * radius;
+            Console.WriteLine("The Area = " + area);
+
+            Console.ReadLine();
+        }
+    }
+}
