5ebb74358185a7c4716ea414f76e88ac66ba6d2a
Jim Baker <jim.k.baker@icloud.com>
2017-03-04 10:18:31 -0600

    HelloMVC commit Class 5 Prep work

---
+++ SimpleClasses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Jeep";
+            myCar.Model = "Wrangler";
+            myCar.Year = 1997;
+            myCar.Color = "Red";
+
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myCar.Make, 
+                myCar.Model, 
+                myCar.Year, 
+                myCar.Color);
+
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+
+            Console.ReadLine();
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 3500.0M;
+
+            return carValue;
+        }
+
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+
+            if (Year > 2007)
+                carValue = 16000;
+            else
+                carValue = 3500;
+
+            return carValue;
+        }
+    }
+
+
+}
