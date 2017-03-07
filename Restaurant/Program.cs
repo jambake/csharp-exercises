ad71b42e145e4709e283cebb96181e4d10245ff2
Jim Baker <jim.k.baker@icloud.com>
2017-03-07 12:15:26 -0600

    initial commit of Restaurant Studio

---
+++ Restaurant/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu newMenu = new Menu();
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Menu
+    {
+        public Dictionary<string, Object> MenuItems { get; set; }
+
+        public Menu()
+        {
+
+        }
+        public Menu (Dictionary<string, Object> menuItems)
+        {
+            MenuItems = menuItems;
+        }
+    }
+
+    class MenuItem
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        public MenuItem (string name, double price, string description, string category)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            Category = category;
+        }
+    }
+}
