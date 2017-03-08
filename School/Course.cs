52f59758bee932afeb56ab593e993fc63c966dd0
Jim Baker <jim.k.baker@icloud.com>
2017-03-07 19:27:21 -0600

    review of properties and constructors

---
+++ School/Course.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class Course
+    {
+        private string courseName;
+        private int credits;
+        private Dictionary<Student, string> numOfStudents;
+
+        public string CourseName
+        {
+            get { return courseName; }
+            set { courseName = value; }
+        }
+        public int Credits
+        {
+            get { return credits; }
+            internal set { credits = value; }
+        }
+        public Dictionary<Student, string> NumOfStudents
+        {
+            get { return numOfStudents; }
+            set { numOfStudents = value; }
+        }
+    }
+}
---
+++ School/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class Student
+    {
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+        
+        //Worst when made public - most students won't initialize with credits or gpa - extra work for user
+        private Student(string name, int studentId, int numberOfCredits, double gpa)
+        {
+            Name = name;
+            StudentId = studentId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+        //Better - but studentId should not be created by user but rather the constructor
+        //public Student(string name, int studentId)
+        //{
+        //    Name = name;
+        //    StudentId = studentId;
+        //    NumberOfCredits = 0;
+        //    Gpa = 0.0;
+        //}
+
+        public Student(string name, int studentId)
+            : this(name, studentId, 0, 0) { }
+
+        public Student(string name) : this(name, nextStudentId)
+        {
+            nextStudentId++;
+        }
+
+        public override string ToString()
+        {
+            return Name + " (Credits: " + NumberOfCredits +
+                ", GPA: " + Gpa + ")";
+        }
+
+        public override bool Equals(Object o)
+        {
+            //Java example (casting): Student studentObj = (Student) o;
+            Student studentObj = o as Student;
+            return StudentId == studentObj.StudentId;
+        }   //if overriding .Equals, you're expected to override .GetHashCode as well
+    }
+}
